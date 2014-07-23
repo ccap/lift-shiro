5edab0b3d131727237485ed2bcf0724981953f18
Peter Brant <peter.brant@gmail.com>
2014-07-23 22:10:52 +0200

    Hack build for Lift 3 and sbt 0.13
    (also, don't build example project)

--- project/LiftModule.scala
+++ project/LiftModule.scala
@@ -5,7 +5,4 @@ object LiftModuleBuild extends Build {
   val liftVersion = SettingKey[String]("liftVersion", "Full version number of the Lift Web Framework")
 
   val liftEdition = SettingKey[String]("liftEdition", "Lift Edition (short version number to append to artifact name)")
-
-  val project = Project("LiftModule", file("."))
-
 }
